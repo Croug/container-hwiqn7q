e68b2c4b56e3edd8a84bee099ac12974746f6722
Jordan <jlaprise@blakwurm.com>
2023-10-24 15:38:51 -0800

    Add bounce endpoint

--- src/web/app.py
+++ src/web/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests  # You may need to install this library using pip
 from db import get_db, close_db
 import sqlalchemy
 from logger import log
@@ -6,27 +7,19 @@ from logger import log
 app = Flask(__name__)
 app.teardown_appcontext(close_db)
 
-
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return "Hello World!"
 
-
-@app.route("/health")
-def health():
-    log.info("Checking /health")
-    db = get_db()
-    health = "BAD"
-    try:
-        result = db.execute("SELECT NOW()")
-        result = result.one()
-        health = "OK"
-        log.info(f"/health reported OK including database connection: {result}")
-    except sqlalchemy.exc.OperationalError as e:
-        msg = f"sqlalchemy.exc.OperationalError: {e}"
-        log.error(msg)
-    except Exception as e:
-        msg = f"Error performing healthcheck: {e}"
-        log.error(msg)
-
-    return health
+@app.route("/bounce")
+def bounce():
+    url = request.args.get('url')  # Get the URL from the query parameters
+    if url:
+        try:
+            response = requests.get(url)  # Make the GET request
+            response.raise_for_status()  # Check if the request was successful
+            return response.text  # Return the body of the response
+        except requests.RequestException as e:
+            return f"An error occurred: {e}", 400  # Return an error message if the request fails
+    else:
+        return "No URL provided", 400  # Return an error message if no URL is provided
